f457e55a12ad214a64659dc077d1f47894841a56
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-08-17 11:25:49 -0300

    Finishing TeacherItem component

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+function TeacherItem() {
+    return (
+        <article className="teacher-item">
+            <header>
+                <img src="https://avatars3.githubusercontent.com/u/22963494?s=460&u=33011adcaad4978ac765923873d76d29533bb3e1&v=4" alt="Gabriel Klein" />
+                <div>
+                    <strong>Gabriel Klein</strong>
+                    <span>Química</span>
+                </div>
+            </header>
+
+            <p>
+                Entusiasta das melhores tecnologias de química avançada.
+                <br /><br />
+                Apaixonado por explodir coisas em laboratórios e mudar a vida das pessoas
+                através de experiências. Mais de 200.000 pessoas já passaram por um das
+                minhas explosões.
+            </p>
+
+            <footer>
+                <p>
+                    Preço/hora
+                    <strong>R$ 80,00</strong>
+                </p>
+                <button type="button">
+                    <img src={whatsappIcon} alt="Whatsapp" />
+                    Entrar em contato
+                </button>
+            </footer>
+        </article>
+    );
+}
+
+export default TeacherItem;
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
 
@@ -29,32 +29,11 @@ function TeacherList() {
             </PageHeader>
 
             <main>
-                <article className="teacher-item">
-                    <header>
-                        <img src="https://avatars3.githubusercontent.com/u/22963494?s=460&u=33011adcaad4978ac765923873d76d29533bb3e1&v=4" alt="Gabriel Klein" />
-                        <div>
-                            <strong>Gabriel Klein</strong>
-                            <span>Química</span>
-                        </div>
-                    </header>
-
-                    <p>
-                        Título do texto
-                        <br /><br />
-                        Texto
-                    </p>
-
-                    <footer>
-                        <p>
-                            Preço/hora
-                            <strong>R$ 80,00</strong>
-                        </p>
-                        <button type="button">
-                            <img src={whatsappIcon} alt="Whatsapp" />
-                            Entrar em contato
-                        </button>
-                    </footer>
-                </article>
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
             </main>
         </div>
     )
