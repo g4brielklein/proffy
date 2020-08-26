925f7f551dec68d5811b1626ef07ab6d685b3d97
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-08-26 12:07:06 -0300

    Creating subject and time filters

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -30,8 +30,10 @@ export default class ClassesControler {
             .whereExists(function() {
                 this.select('class_schedule.*')
                     .from('class_schedule')
-                    .whereRaw('`class_schedule`.`class_id`. = `classes_id`')
+                    .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
                     .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+                    .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+                    .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
             })
             .where('classes.subject', '=', subject)
             .join('users', 'classes.user_id', '=', 'users.id')
@@ -86,6 +88,8 @@ export default class ClassesControler {
         
             return response.status(201).send();
         } catch (err) {
+            console.log(err);
+
             await trx.rollback();
     
             return response.status(400).json({
